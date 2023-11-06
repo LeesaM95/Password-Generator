df9238383d2623ca1add85da616dd4af87c1df9b
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2023-11-06 14:57:25 -0500

    Attempt number 1 to write password generator code using a tutorial
    tutorial by Code Radiance on youtube.

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,38 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(length, numbers, uppChar, lowChar, specChar, noDuplicates) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+
+  let characters = '';
+
+  if(numbers){
+    characters += '0,1,2,3,4,5,6,7,8,9'
+  }
+  if(uppChar){
+    characters += 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'
+  }
+  if(lowChar){
+    characters += 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+  }
+  if(specChar){
+    characters += '!,@,#,$,%,^,&,*,(,)'
+  }
+
+  let passwordArray = [];
+
+  while(passwordArray.length < length) {
+  const character = characters[Math.floor(Math.random() * characters.length)];
+
+  if(!noDuplicates || !passwordArray.includes(character)){
+    passwordArray.push(character)
+  }
+  passwordArray.push(character)
+}
+
+  return passwordArray.join('');
 
 }
 
