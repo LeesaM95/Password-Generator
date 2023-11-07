5969ec8d4618b7148f3186fc34f9efd3974dbf5f
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2023-11-06 21:33:04 -0500

    added Math for randomizer. removed elements that weren't needed.  program still not executing.

--- Develop/script.js
+++ Develop/script.js
@@ -6,33 +6,34 @@ var allChars = {
     lowerCase: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z",
     upperCase: "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",
    };
+var passLength = 8;
 
 
 function generatePassword(){
     let passInfo = "";
     const passChars = [];
-    
+
     let charaAmount = window.prompt("Enter the number of characters you want for your password. (Passwords MUST be between 8 and 128 characters.");
       if (charaAmount >= 8 && charaAmount < 129) {
           const getNumber = window.confirm("Would you like to include numbers?");
           if (getNumber) {
               passInfo += allChars.numbers;
-              passChars.push(getRandomChar(allChars.numbers));
+              passChars.push(allChars.numbers);
           };
           const getSpecChar = window.confirm("Would you like to include special characters?");
           if (getSpecChar) {
             passInfo += allChars.symbols;
-            passChars.push(getRandomChar(allChars.symbols));
+            passChars.push(allChars.symbols);
           };
           const getLower = window.confirm("Would you like to include lowercase letters?");
           if (getLower) {
             passInfo += allChars.lowerCase;
-            passChars.push(getRandomChars(allChars.lowerCase));
+            passChars.push(allChars.lowerCase);
           };
           const getUpper = window.confirm("Would you like to include uppercase letters?");
           if (getUpper) {
             passInfo += allChars.upperCase;
-            passChars.push(getRandomChar(allChars.upperCase));
+            passChars.push(allChars.upperCase);
           };
           if (!passInfo) {
             window.alert("Invalid. You must choose at least one option. Try again.");
@@ -40,28 +41,11 @@ function generatePassword(){
           };
           
           while (passChars.length < charaAmount) {
-            passChars.push(getRandomChars(passInfo));
-          };
-          //return password character array concat-style to a string
-
-          //if user response is invalid
-
-          //alert user
-
-          //return to initial state
-
+            passChars.push((passInfo));
+          }; 
       }
 
-
-// 2. Validate the input. 
-   
-
-
-// 3. Generate password based on criteria.
-   
-
-// 4. Display password to the page.
-  
+          let generateBtn = document.querySelector("#generate");
 }
 
 // Write password to the #password input
@@ -71,7 +55,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+  for (var i = 0; i <= passLength; i++) {
+    var password = Math.floor(Math.random() * allChars.length);
+    password += allChars.substring(allChars, allChars +1);
+  };
+
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
